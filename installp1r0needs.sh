eb0f4ec4617ee235ce4d8ba6ca9ab9f6a61e2c7f
p1r0 <p1r0@insomnia247.nl>
2017-04-25 19:19:46 -0500

    agregando dos paquetes para px

--- installp1r0needs.sh
+++ installp1r0needs.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-apt-get install vim tmux zsh nmap git mosh autossh socat
+apt-get install vim tmux zsh nmap git mosh autossh socat sshuttle proxychains
+cp vimrc  /etc/vim
+cp tmux.conf ~p1r0/.tmux.conf && chown p1r0.p1r0 ~p1r0/.tmux.conf
+cp tmux.conf ~/.tmux.conf
